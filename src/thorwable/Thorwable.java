264a55d245c980e70456e5f825eb70cbb72758f4
INT105 <INT105@10.4.22.132>
2018-03-28 10:10:12 +0700

    Catch my exception

--- src/thorwable/Thorwable.java
+++ src/thorwable/Thorwable.java
@@ -4,52 +4,53 @@ import java.util.Scanner;
 
 public class Thorwable {
 
-    public static void main(String[] args) {
-        int x = 0;
-        int y = 0;
-//        for (int i = 0; y != -999; i++) {
+    public static void main(String[] args) throws MyException {
+//        int x = 0;
+//        int y = 0;
+//        while (y != -999) {
+//            System.out.println("Compute X/Y");
 //            Scanner scan = new Scanner(System.in);
 //            System.out.print("X: ");
 //            x = scan.nextInt();
 //            System.out.print("Y: ");
 //            y = scan.nextInt();
-//            System.out.println(devideMe(x , y));
+//            try {
+//                System.out.println("from devideMe X/Y = " + x + '/' + y + " = " + devideMe(x, y));
+//            } catch (ArithmeticException ex) {
+//                System.err.println("!!!!! Y cannot be 0 !!!!");
+//            }
+//
+//            System.out.println("from devideMe2 X/Y = " + x + '/' + y + " = " + devideMe2(x, y));
 //        }
-        while (y != -999) {
-            System.out.println("Compute X/Y");
-            Scanner scan = new Scanner(System.in);
-            System.out.print("X: ");
-            x = scan.nextInt();
-            System.out.print("Y: ");
-            y = scan.nextInt();
-            try {
-                System.out.println("from devideMe X/Y = " + x + '/' + y + " = " + devideMe(x, y));
-            } catch (ArithmeticException ex) {
-                System.err.println("!!!!! Y cannot be 0 !!!!");
-            }
+//    }
+//
+//    public static int devideMe(int x, int y) {
+//        return x / y;
+//    }
+//
+//    public static int devideMe2(int x, int y) {
+//        int result;
+//        try {
+//            result = x / y;
+//        } catch (ArithmeticException ex) {
+//            result = 0;
+//        }
+//        return result;
 
-            System.out.println("from devideMe2 X/Y = " + x + '/' + y + " = " + devideMe2(x, y));
-        }
-    }
-
-    public static int devideMe(int x, int y) {
-        return x / y;
-    }
-
-    public static int devideMe2(int x, int y) {
-        int result;
         try {
-            result = x / y;
-        } catch (ArithmeticException ex) {
-            result = 0;
+           exp("INT105");
+        } catch(MyException ex) {
+            System.out.println(ex.getMessage());
         }
-        return result;
+        
+        
     }
 
     public static void exp(String subject) throws MyException {
         if (subject.equals("INT102")) {
-            throw new MyException("Subject Error");
+            MyException obj = new MyException("Exception: I have got \"A\"");
+            throw obj;
         }
-        System.out.println("Stop method");
+        System.out.println("I will get \"A\"");
     }
 }
