acee827b3b0d552998f59ff3db726f594870d679
INT105 <INT105@10.4.22.132>
2018-03-28 09:48:07 +0700

    create my own Exception

--- src/thorwable/MyException.java
+++ src/thorwable/MyException.java
@@ -1,11 +1,19 @@
 package thorwable;
 
-
 public class MyException extends Exception {
+
     public MyException(String msg) {
         super(msg);
     }
+
     public MyException() {
         super();
+    }
+
+    public static void exp(String subject) throws MyException {
+        if(subject.equals("INT102")) {
+            throw new MyException("Subject Error");
+        }
+        System.out.println("Stop method");
     }
 }
