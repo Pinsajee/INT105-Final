1f18016d6fb6c55bb5f6e4fca316041638105335
INT105 <INT105@10.4.22.132>
2018-04-04 11:32:24 +0700

    My SQL Code

---
+++ AhaShopG.4/src/ahashopg/pkg4/AhaShopG4.java
@@ -0,0 +1,22 @@
+package ahashopg.pkg4;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class AhaShopG4 {
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("org.apache.derby.jdbc.ClientDriver");
+        Connection connect = DriverManager.getConnection("jdbc:derby://localhost:1527/aha_shopG4", "app", "app");
+
+        Statement state = connect.createStatement();
+
+//        state.executeUpdate("INSERT INTO product VALUES (24,'Watch',200)");
+//        state.executeUpdate("UPDATE product SET price=2000 WHERE product_id=24");
+        state.executeQuery("SELECT * FROM order_item");
+        connect.close();
+    }
+
+}
