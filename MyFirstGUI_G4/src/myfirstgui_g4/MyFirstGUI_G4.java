35792fb73b750e9939fc87ba3090ec3b50635de2
INT105 <INT105@10.4.22.132>
2018-05-02 10:10:25 +0700

    GUI

---
+++ MyFirstGUI_G4/src/myfirstgui_g4/MyFirstGUI_G4.java
@@ -0,0 +1,45 @@
+package myfirstgui_g4;
+
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class MyFirstGUI_G4 {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("My first app");
+        frame.setSize(600, 400);
+        frame.setLocationRelativeTo(null);
+
+        JPanel panal01 = new JPanel();
+        
+        JButton buttonOK = new JButton("OK");
+        
+        JTextField tfCourseID = new JTextField(10);
+        JLabel lbCourseID = new JLabel("Course ID");
+        
+        JTextField tfCredit = new JTextField(5);
+        JLabel lbCredit = new JLabel("Credit");
+        
+        JTextField tfGrade = new JTextField(5);
+        JLabel lbGrade = new JLabel("Grade");
+        
+        panal01.add(lbCourseID);
+        panal01.add(tfCourseID);
+        panal01.add(lbCredit);
+        panal01.add(tfCredit);
+        panal01.add(lbGrade);
+        panal01.add(tfGrade);
+
+        panal01.add(buttonOK);
+
+        frame.add(panal01);
+
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+}
