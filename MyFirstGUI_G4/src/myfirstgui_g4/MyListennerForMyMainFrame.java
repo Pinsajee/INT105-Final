960c04d54f61f39315497f8970243c6191672304
INT105 <INT105@10.4.22.132>
2018-05-02 12:11:35 +0700

    create file

--- MyFirstGUI_G4/src/myfirstgui_g4/MyListennerForMyMainFrame.java
+++ MyFirstGUI_G4/src/myfirstgui_g4/MyListennerForMyMainFrame.java
@@ -3,6 +3,12 @@ package myfirstgui_g4;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class MyListennerForMyMainFrame implements ActionListener {
@@ -18,11 +24,26 @@ public class MyListennerForMyMainFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
 //        System.out.println(frame.getTfCourseID().getText());
-    JTextField temp = frame.getTfCourseID();
-    System.out.println(temp.getText());
-    
-    frame.getTextOutput().append(frame.getTfCourseID().getText());
-    
+    JTextField courseID = frame.getTfCourseID();
+    JTextField credit = frame.getTfCredit();
+    JTextField grade = frame.getTfGrade();
+    JTextArea output = frame.getTextOutput();
+    output.append(frame.getTfCourseID().getText() + "\t" + frame.getTfCredit().getText() + "\t" + frame.getTfGrade().getText() + "\n");
+
+    File f = new File("file/Mygrade.txt");
+    FileWriter fw = null;
+        try {
+            fw = new FileWriter(f);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());;
+        } finally {
+        try {
+            fw.close();
+        } catch (Exception ex) {
+            
+        }
+        }
+        System.out.println(output.getText());
     }
     
 }
